17c721ca2537cd4124ac72f68bb97b9e65bd148c
Pan Otlet <otlet0@gmail.com>
2014-04-02 08:59:07 +0200

    Gotowe

--- C++/Obiektówka/zad6_test.cpp
+++ C++/Obiektówka/zad6_test.cpp
@@ -23,7 +23,7 @@ class ul
   friend ul pom(ul &u_1,ul &u_2);
   friend ul pod(ul &u_1,ul &u_2);
   friend ul dod(ul &u_1,ul &u_2);
-  friend ul odejmij(ul &u_1,ul &u_2);
+  friend ul ode(ul &u_1,ul &u_2);
 
 };
 
@@ -63,38 +63,39 @@ ul pod(ul &u_1,ul &u_2)
       u_3.mianownik=u_1.mianownik*u_2.licznik;
       return u_3;
     }
-ul dod (ul u_1, ul u_2)
-  {
-      u u_3;
-      int a=abs(u_1,mianownik);
-      int b=abs(u_2,mianownik);
-      int c=a, d=b;
-      while (a!=b)
-            if (a-b>0)
-               a=a-b;
-            else
-                b=b-a;
-      int nww=)c*d)/a;
+
+ul dod(ul &u_1,ul &u_2)
+    {
+      ul u_3;
+      int a=abs(u_1.mianownik);
+      int b=abs(u_2.mianownik);
+      int c=a,d=b;
+      	while(a!=b)
+      		if(a-b>0)
+      			a=a-b;
+      		else
+      			b=b-a;
+      int nww=(c*d)/a;	
       u_3.mianownik=nww;
       u_3.licznik=nww/c*u_1.licznik+nww/d*u_2.licznik;
       return u_3;
-  }
-u odejmij (ulamek u_1, ulamek u_2)
-  {
-      u u_3;
-      int a=abs(u_1,mianownik);
-      int b=abs(u_2,mianownik);
-      int c=a, d=b;
-      while (a!=b)
-            if (a-b>0)
-               a=a-b;
-            else
-                b=b-a;
-      int nww=)c*d)/a;
+    }
+ul ode(ul &u_1,ul &u_2)
+    {
+      ul u_3;
+      int a=abs(u_1.mianownik);
+      int b=abs(u_2.mianownik);
+      int c=a,d=b;
+      	while(a!=b)
+      		if(a-b>0)
+      			a=a-b;
+      		else
+      			b=b-a;
+      int nww=(c*d)/a;	
       u_3.mianownik=nww;
       u_3.licznik=nww/c*u_1.licznik-nww/d*u_2.licznik;
       return u_3;
-  }
+    }
 
 
 int main()
@@ -102,48 +103,70 @@ int main()
   ul ul1(4,5);
   ul ul2(1,7);
   int licz,mian,w;
-  cout<<"pierwszy ulamek : ";
+   cout<<"pierwszy ulamek : ";
   ul1.wypisz();
-  cout<<'\n';
-  cout<<"poskruconiu:"<<'\n';
+   cout<<'\n';
+   cout<<"poskruconiu:"<<'\n';
   ul1.skc();
   ul1.wypisz();
    cout<<'\n';
-  cout<<"drugi ulamek : ";
+   cout<<"drugi ulamek : ";
   ul2.wypisz();
-  cout<<'\n';
-  cout<<"poskruconiu: ";
+   cout<<'\n';
+   cout<<"poskruconiu: ";
   ul2.skc();
   ul2.wypisz();
    cout<<'\n';
-  cout<<"wynik mnozenia u1*u2 wynosi : ";
+   cout<<"wynik mnozenia u1*u2 wynosi : ";
   ul ul5=pom(ul1,ul2);
   ul5.wypisz();
-  cout<<'\n';
-  cout<<"poskruconiu: ";
+   cout<<'\n';
+   cout<<"poskruconiu: ";
   ul5.skc();
   ul5.wypisz();
    cout<<'\n';
-  cout<<"wynik mnozenia u1/u2 wynosi : ";
-  ul5=pom(ul1,ul2);
+   cout<<"wynik dzielienia u1/u2 wynosi : ";
+  ul5=pod(ul1,ul2);
   ul5.wypisz();
    cout<<'\n';
-     cout<<"poskruconiu: ";
+   cout<<"poskruconiu: ";
   ul5.skc();
   ul5.wypisz();
    cout<<'\n';
-  cout<<"wynik mnozenia u2/u1 wynosi : ";
-  ul5=pom(ul1,ul2);
+   cout<<"wynik dzielienia u2/u1 wynosi : ";
+  ul5=pod(ul2,ul1);
   ul5.wypisz();
-  cout<<'\n';
-    cout<<"poskruconiu: ";
+   cout<<'\n';
+   cout<<"poskruconiu: ";
   ul5.skc();
   ul5.wypisz();
    cout<<'\n';
-   //cout<<'\n';
+   cout<<"wynik dodawania u1+u2 wynosi : ";
+   ul5=dod(ul1,ul2);
+  ul5.wypisz();
+   cout<<'\n';
+   cout<<"poskruconiu: ";
+  ul5.skc();
+  ul5.wypisz();
+   cout<<'\n';
+   cout<<"wynik odejmowania u1-u2 wynosi : ";
+  ul5=ode(ul1,ul2);
+  ul5.wypisz();
+   cout<<'\n';
+   cout<<"poskruconiu: ";
+  ul5.skc();
+  ul5.wypisz();
+   cout<<'\n'; 
+   cout<<"wynik odejmowania u2-u1 wynosi : ";
+  ul5=ode(ul2,ul1);
+  ul5.wypisz();
+   cout<<'\n';
+   cout<<"poskruconiu: ";
+  ul5.skc();
+  ul5.wypisz();
   
-  cout<<'\n';
-  cout<<"podaj licznik 3: ";
+   cout<<'\n';
+   cout<<"podaj licznik 3: ";
   cin>>licz;
   do
   {
@@ -159,45 +182,69 @@ int main()
      cin>>mian;         
   }while(mian==0);
   ul ul4(licz,mian);  
-  cout<<"twoj ulamek 3:";
+   cout<<"twoj ulamek 3:";
   ul3.wypisz();
-  cout<<'\n';
-  cout<<"poskruconiu: ";
+   cout<<'\n';
+   cout<<"poskruconiu: ";
   ul3.skc();
   ul3.wypisz();
    cout<<'\n';
-  cout<<"twoj ulamek 4:";
+   cout<<"twoj ulamek 4:";
   ul4.wypisz();
   ul5=pom(ul3,ul4);
-  cout<<'\n';
+   cout<<'\n';
     cout<<"poskruconiu: ";
   ul4.skc();
   ul4.wypisz();
    cout<<'\n';
-  cout<<"wynik mnozenia u3*u4 wynosi : ";
+   cout<<"wynik mnozenia u3*u4 wynosi : ";
   ul5.wypisz();
-  cout<<'\n';
-    cout<<"poskruconiu: ";
+   cout<<'\n';
+   cout<<"poskruconiu: ";
   ul5.skc();
   ul5.wypisz();
    cout<<'\n';
   ul5=pod(ul3,ul4);
-  cout<<"wynik mnozenia u3/u4 wynosi : ";
+   cout<<"wynik dzielienia u3/u4 wynosi : ";
   ul5.wypisz();
    cout<<'\n';
-     cout<<"poskruconiu: ";
+   cout<<"poskruconiu: ";
   ul5.skc();
   ul5.wypisz();
    cout<<'\n';
   ul5=pod(ul4,ul4);
-  cout<<"wynik mnozenia u4/u3 wynosi : ";
+   cout<<"wynik dzielienia u4/u3 wynosi : ";
   ul5.wypisz();
-  cout<<'\n';
-  cout<<"poskruconiu: ";
+   cout<<'\n';
+   cout<<"poskruconiu: ";
   ul5.skc();
   ul5.wypisz();
    cout<<'\n';
-  cout<<"\n\n";
+   cout<<"wynik dodawania u3+u4 wynosi : ";
+   ul5=dod(ul3,ul4);
+  ul5.wypisz();
+   cout<<'\n';
+   cout<<"poskruconiu: ";
+  ul5.skc();
+  ul5.wypisz();
+   cout<<'\n';
+   cout<<"wynik odejmowania u3-u4 wynosi : ";
+  ul5=ode(ul3,ul4);
+  ul5.wypisz();
+   cout<<'\n';
+   cout<<"poskruconiu: ";
+  ul5.skc();
+  ul5.wypisz();
+   cout<<'\n'; 
+   cout<<"wynik odejmowania u4-u3 wynosi : ";
+  ul5=ode(ul4,ul3);
+  ul5.wypisz();
+   cout<<'\n';
+   cout<<"poskruconiu: ";
+  ul5.skc();
+  ul5.wypisz();
+   cout<<'\n';
+   cout<<"\n\n";
   
     return 0;
 }
