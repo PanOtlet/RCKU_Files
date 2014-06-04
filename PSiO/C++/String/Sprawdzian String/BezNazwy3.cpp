e51a99b5c9d052368f06a9b676bbe20a275a4e18
Pan Otlet <otlet0@gmail.com>
2014-06-04 08:16:11 +0200

    Trollo

--- PSiO/C++/String/Sprawdzian String/BezNazwy3.cpp
+++ PSiO/C++/String/Sprawdzian String/BezNazwy3.cpp
@@ -4,14 +4,18 @@ using namespace std;
 
 int main()
 {
-    string sl;
-    cout<<"Podaj zdanie";
+    string sl,big;
+    int a=0,b=0;
+    cout<<"Podaj slowo: ";
     cin>>sl;
+    cout<<"Podaj slowo, ale tym razem z tylko duzych liter: ";
+    cin>>big;
     for (int i=0; i<=sl.length(); i++)
-        if (sl[i]==sp[0])
-           cout<<'\n';
+        if (sl[i]==big[i])
+           a++;
         else
-            cout<<sl[i];    
+            b++;
+    cout<<"W slowie jest "<<a-1<<" duzych liter i "<<b-1<<" malych liter."; 
     system("pause");
     return 0;
 }
