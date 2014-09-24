6a6bf2133b85136d595c2fff84e85acf5f7a478f
Pan Otlet <otlet0@gmail.com>
2014-09-24 13:57:34 +0200

    Zadanie 2

--- PSiO/C++/Operacje na plikach/Pliki zadania i dane/BezNazwy2.cpp
+++ PSiO/C++/Operacje na plikach/Pliki zadania i dane/BezNazwy2.cpp
@@ -6,25 +6,38 @@ using namespace std;
 
 int main()
 {
- int liczba,i=0,max=0,min=9999;
- string str1,str2;
- fstream plik,wyniki;
- plik.open("lustro.txt",ios::in);
- wyniki.open("wyniki_2.txt",ios::out);
-  if( plik.good() == true )
-  {
-   while(!plik.eof())
-   {
-    plik >> str1;
-    plik >> str2;
-    if (str1[str1.length()]==str2[str2.length()])
-       for(int i=0;i<str1.length();i++)
-   	   if(str1[i]==str2[str2.size()-i-1])
-   	{
- 	 cout<<str1<<","<<str2<<endl;
-    }
-   }    
-   plik.close();
- }
- return 0;
+	string str1,str2,nazwa_p="lustro.txt";
+	fstream plik;
+		plik.open(nazwa_p.c_str());
+        if( plik.good() == true )
+        {
+            while(!plik.eof())
+            {
+ 			   bool ch=true;
+		       plik >> str1;
+ 			   plik >> str2;
+ 			   if(str1.size()==str2.size())
+ 			   	   for(int i=0;i<str1.size();i++)
+  	   		           if(str1[i]==str2[str2.size()-1-i])
+  	   		           ;
+  	   		           else
+  	   		           {
+  	   		           	ch=false;
+  	   		           	break;
+  	   		           }
+  	   		   else
+					ch=false;      
+               if(ch==true)
+		   		 cout<<str1<<", "<<str2<<'\n';   
+ 			   {
+            }
+            }
+     
+            plik.close();
+         }
+         else
+		cout<<"siewo"<<'\n';
+	
+	
+	return 0;
 }
